feb4b1a6f937e59ecfc12520374ab1247d8e709d
asrulhaque <107788929+asrulhaque@users.noreply.github.com>
2022-06-19 13:30:46 +0530

    Add files via upload

---
+++ ArrayEvensum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayEvensum {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        //int n =sc.nextInt();
+        //int[] arr= new int[100];
+        int sum =0;
+
+        for(int i=0; i<=50; i++) {
+
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+
+            System.out.println(sum);
+    }
+}
